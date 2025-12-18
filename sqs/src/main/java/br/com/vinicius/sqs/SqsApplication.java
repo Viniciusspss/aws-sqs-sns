70866a1b397d2af3e5589cf94ad8e7e74f790d59
Vinicius Sátiro <vinicius.satiro@cwi.com.br>
2025-12-18 12:04:26 -0300

    add sqs project

---
+++ sqs/src/main/java/br/com/vinicius/sqs/SqsApplication.java
@@ -0,0 +1,24 @@
+package br.com.vinicius.sqs;
+
+import io.awspring.cloud.sqs.operations.SqsTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsApplication.class, args);
+	}
+
+    @Autowired
+    private SqsTemplate sqsTemplate;
+
+    @Override
+    public void run(String... args) throws Exception {
+        var SQS = "https://localhost.localstack.cloud:4566/000000000000/minha-fila";
+        sqsTemplate.send(SQS, "meu valor de start");
+    }
+}
---
+++ sqs/src/test/java/br/com/vinicius/sqs/SqsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
